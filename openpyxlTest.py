7f70935f5a3e1620be6b9e9b60164e96cb540515
AlmirFonseca <almirfonseca60@gmail.com>
2022-05-25 19:55:11 -0300

    Adicione a geração de um cabeçalho para a tabela

--- openpyxlTest.py
+++ openpyxlTest.py
@@ -1,4 +1,6 @@
 from openpyxl import Workbook
+from openpyxl.styles.alignment import Alignment
+
 import random
 
 # Cria um Workbook
@@ -11,6 +13,8 @@ ws.title = "Dashboard"
 
 # Para uma pessoa com 22 ações de 9.23 dólares da stone, que valem 22x9.23=203,06:
 # E algo semelhante em relação à amazon
+header = ["Nome", "Código/Ticker", "Quantidade", "Valor unitário", "Valor Total"]
+
 financial_data = [["StoneCro Ltd.", "STNE", 22, 9.23, 203.06],
         ["Amazon.com, Inc", "AMZN", 2, 2127.07, 4252.14]]
 
@@ -18,11 +22,22 @@ financial_data = [["StoneCro Ltd.", "STNE", 22, 9.23, 203.06],
 row_offset = 1
 column_offset = 1
 
+# Cria o cabeçalho de uma tabela
+ws.merge_cells(start_row=row_offset+1, start_column=column_offset+1, end_row=row_offset+1, end_column=column_offset+len(header)) # Merge as células imediatamente acima da tabela
+ws.cell(row=row_offset+1, column=column_offset+1, value="Ativos") # Define o texto da célula
+ws.cell(row=row_offset+1, column=column_offset+1).alignment = Alignment(horizontal="center") # Centraliza o conteúdo
+
+for head in header:
+    ws.cell(row=row_offset+2, column=header.index(head)+column_offset+1, value=head)
+
 # Transcreve uma lista de listas para a worksheet
 for row_num, row_content in enumerate(financial_data):
     for data in row_content:
-        ws.cell(row=row_num + row_offset + 1, column=row_content.index(data) + column_offset + 1, value=data)
+        ws.cell(row=row_num+row_offset+3, column=row_content.index(data)+column_offset+1, value=data)
 
 filename = "./rascunhos/Teste-" + str(random.randint(1, 10000)) + ".xlsx"
 # Salva o arquivo
 wb.save(filename)
+
+
+# DECIDIR ENTRE OS DOIS MODELOS DE DICT E, CONSEQUENTEMENTE, ENTRE UM OUTPUT COM 1 OU 2 TABELAS
