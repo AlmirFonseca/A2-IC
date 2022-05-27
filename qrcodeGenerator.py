9cdb44a8b0b269a734d7603ba5c9ca9792de645f
AlmirFonseca <almirfonseca60@gmail.com>
2022-05-27 16:12:14 -0300

    Adicione comentários e altere o padrão de exibição do valor total da carteira

--- qrcodeGenerator.py
+++ qrcodeGenerator.py
@@ -1,9 +1,28 @@
 import qrcode
 
+# Função responsável por gerar um QR Code contendo o valor total da carteira
 def gerar_qrcode(valor_carteira):
-    qr_content = "O valor total da sua carteira é: R$" + str(valor_carteira)
-    qr_filename = "QRCode.png"
-    qr_object = qrcode.make(qr_content)
-    qr_img = qr_object.save(qr_filename)
+    # Gera uma string com o valor da carteira, mas com o padrão de pontuação americano
+    string_valor_carteira = "{:,.2f}".format(round(valor_carteira, 2))
 
+    # Altera as "," por ".", e vice-versa, alterando a string para o padrão brasileiro
+    lista_auxiliar = list(string_valor_carteira)
+    for index, character in enumerate(lista_auxiliar):
+        if character == ",":
+            lista_auxiliar[index] = "."
+        elif character == ".":
+            lista_auxiliar[index] = ","
+    string_valor_carteira = "".join(lista_auxiliar)
+
+    # Concatena o valor da carteira à uma frase, montando o conteúdo do qr code
+    qr_content = "O valor total da sua carteira é: R$ " + str(string_valor_carteira)
+    # Define o nome do arquivo a ser gerado
+    qr_filename = "QRCode.png"
+
+    # Cria um imagem com o qrcode e insere nele o conteúdo presente em "qr_content"
+    img = qrcode.make(qr_content)
+    # Salva a imagem
+    img.save(qr_filename)
+
+    # Retorna o nome do arquivo, que é o caminho relativo até ele
     return qr_filename
