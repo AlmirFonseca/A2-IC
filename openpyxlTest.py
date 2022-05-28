ce09cf1bffaca7bdd1b4e3d6ee48ebd9d6b73379
AlmirFonseca <almirfonseca60@gmail.com>
2022-05-28 16:51:36 -0300

    Adicione a geração automática de 3 gráficos, a partir das finanças atuais e da base que comtempla o ano todo

--- openpyxlTest.py
+++ openpyxlTest.py
@@ -4,9 +4,16 @@ from openpyxl.styles.alignment import Alignment
 from openpyxl.drawing.image import Image
 from openpyxl.utils import get_column_letter
 
+import yfinance as yf
+import math
+import pandas as pd
+import os
+from datetime import datetime
+
 import qrcodeGenerator
 
-import random
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (15,6)
 
 # Translada a origem das linhas da tabela segundo o valor definido por "row_offset"
 def row_adjust(row_value_without_offset):
@@ -15,6 +22,11 @@ def row_adjust(row_value_without_offset):
 # Translada a origem das colunas da tabela segundo o valor definido por "column_offset"
 def col_adjust(column_value_without_offset):
     return column_value_without_offset + column_offset
+
+def first_non_nan(list_values):
+    for item in list_values:
+        if math.isnan(float(item)) == False:
+            return item
 
 # Cria um Workbook
 wb = Workbook()
@@ -34,30 +46,51 @@ header = ["Nome", "Código/Ticker", "Tipo", "Quantidade", "Valor unitário", "Va
 # Lista das larguras de cada coluna da tabela
 column_widths = [40, 15, 15, 15, 25, 25]
 
-# Exemplo da estrutura de uma carteira
+# Carteira gerada a partir da busca através do yfinance
 carteira = {
-    "AMZN":{
-        "ticker": "AMZN",
-        "quantidade": 10,
-        "tipo": "Ação",
-        "nome": "Amazon.com, Inc.",
-        "valor_unitario": 2221.55
-    }, 
-    "STNE":{
-        "ticker": "STNE",
-        "quantidade": 20,
-        "tipo": "Ação",
-        "nome": "StoneCo Ltd.",
-        "valor_unitario": 9.71
-    },   
-    "BRL=X":{
-        "ticker": "BRL=X",
-        "quantidade": 1000,
-        "tipo": "Moeda",
-        "nome": "USD/BRL",
-        "valor_unitario": 4.763
-    }    
+    'AMZN': {
+        'ticker': 'AMZN',
+        'quantidade': 10,
+        'tipo': 'Ação',
+        'nome': 'Amazon.com, Inc.',
+        'valor_unitario': 2302.93,
+        'valor_total': 23029.3
+    },
+    'STNE': {
+        'ticker': 'STNE',
+        'quantidade': 2000,
+        'tipo': 'Ação',
+        'nome': 'StoneCo Ltd.',
+        'valor_unitario': 10.28,
+        'valor_total': 205.6
+    },
+    'USDBRL=X': {
+        'ticker': 'USDBRL=X',
+        'quantidade': 1000,
+        'tipo': 'Moeda',
+        'nome': 'USD/BRL',
+        'valor_unitario': 4.7374,
+        'valor_total': 4737.4
+    },
+    'EURBRL=X': {
+        'ticker': 'EURBRL=X',
+        'quantidade': 1000,
+        'tipo': 'Moeda',
+        'nome': 'EUR/BRL',
+        'valor_unitario': 5.0751,
+        'valor_total': 5075.1
+    },
+    'MSFT': {
+        'ticker': 'MSFT',
+        'quantidade': 10,
+        'tipo': 'Ação',
+        'nome': 'Microsoft Corporation',
+        'valor_unitario': 273.24,
+        'valor_total': 2732.4
+    }
 }
+
+data_history = yf.download("AMZN STNE USDBRL=X EURBRL=X MSFT", period="1y", interval="1d")
 
 # Define o número de linhas e colunas a serem "puladas" a partir do início da worksheet
 row_offset = 1
@@ -78,11 +111,11 @@ valor_total_carteira = 0
 # Itera entre os ativos das certeiras
 for row_num, data_ativo in enumerate(carteira.values(), 3):
     #Calcula o valor total referente a cada ativo e o adiciona ao dicionário
-    valor_total_ativo = data_ativo.get("quantidade") * data_ativo.get("valor_unitario")
-    data_ativo.update({"valor_total_ativo": valor_total_ativo})
+    valor_total = data_ativo.get("quantidade") * data_ativo.get("valor_unitario")
+    data_ativo.update({"valor_total": valor_total})
 
     # Acumula o valor total da carteira
-    valor_total_carteira += valor_total_ativo
+    valor_total_carteira += valor_total
 
     # Transcreve os itens do dicionario para a tabela, ajustando também o formato de exibição dos valores monetários
     ws.cell(row=row_adjust(row_num), column=col_adjust(1), value=data_ativo.get("nome"))
@@ -90,7 +123,7 @@ for row_num, data_ativo in enumerate(carteira.values(), 3):
     ws.cell(row=row_adjust(row_num), column=col_adjust(3), value=data_ativo.get("tipo"))
     ws.cell(row=row_adjust(row_num), column=col_adjust(4), value=data_ativo.get("quantidade"))
     ws.cell(row=row_adjust(row_num), column=col_adjust(5), value=round(data_ativo.get("valor_unitario"), 2)).number_format = "R$ #,###.00"
-    ws.cell(row=row_adjust(row_num), column=col_adjust(6), value=round(data_ativo.get("valor_total_ativo"), 2)).number_format = "R$ #,###.00"
+    ws.cell(row=row_adjust(row_num), column=col_adjust(6), value=round(data_ativo.get("valor_total"), 2)).number_format = "R$ #,###.00"
 
 # Ajusta a largura das colunas da tabela segundo os valores da lista "column_widths"
 for i, column_width in enumerate(column_widths, col_adjust(1)):
@@ -104,8 +137,130 @@ ws.cell(row=row_adjust(len(carteira)+3), column=col_adjust(1)).alignment = Align
 # Imprime o valor total da carteira
 ws.cell(row=row_adjust(len(carteira)+3), column=col_adjust(len(header)), value=round((valor_total_carteira), 2)).number_format = "R$ #,###.00"
         
+# Worksheet "Gráfico 1"
+############################################################################### 
+    
+# Cria uma nova Worksheet chamada "Gráfico 1"
+ws = wb.create_sheet(title="Gráfico 1")
+
+# Declara listas que serão preenchidas a partir da leitura da carteira
+ativos = [] # Armazena o código de cada ativo
+participacao_percentual = [] # Armazena o percentual que representa a participação, em absoluto, de cada ativo no valor da carteira
+
+# Itera os ativos da carteira, adicionando os resultados às listas acima
+for ticker, carteira_ativo in carteira.items():
+    ativos.append(ticker)
+    participacao_percentual.append(carteira_ativo.get("valor_total")/valor_total_carteira*100)
+
+# Converte as duas listas em um pandas.DataFrame
+df_graf1 = pd.DataFrame(index=ativos, data=participacao_percentual)
+# Ordena o DataFrame em ordem decrescente de participação
+df_graf1 = df_graf1.sort_values(df_graf1.columns[0], ascending=False)
+
+# Configura e realiza o plot do gráfico
+fig = df_graf1.plot(kind="bar", zorder=2) # Define os dados a serem plotados
+plt.title("Composição percentual da carteira em relação ao valor absoluto", fontdict={'fontsize': 20}, pad=20) # Define o título
+plt.ylabel("Participação percentual no valor total da carteira", fontdict={'fontsize': 14}, labelpad=10) # Define o rótulo do eixo y
+plt.xlabel("Ativos", fontdict={'fontsize': 14}, labelpad=10) # Define o rótulo do eixo x
+plt.grid(color="#DCDCDC", axis="y", zorder=0) # Configura a exibição de linhas de grade
+fig.get_legend().remove() # Remove as legendas
+
+plt.savefig("grafico_1.png", dpi=75, bbox_inches='tight') # Salva o gráfico gerado em "grafico_1.png"
+
+# Retira a exibição das linhas de grade da planilha
+ws.sheet_view.showGridLines = False
+
+# Abre a imagem gerada que contém o QR code
+img = Image("grafico_1.png")
+
+# Adiciona a imagem à tabela, ancorada sobre a célula de coordenadas (2, 2) -> B2
+ws.add_image(img, ws.cell(2, 2).coordinate)
+
+# Worksheet "Gráfico 2"
+###############################################################################     
+# Cria uma nova Worksheet chamada "Gráfico 2"
+ws = wb.create_sheet(title="Gráfico 2")
+
+# Cria uma "deep copy" do dataframe que contém o histórico dos ativos
+df_graf2 = data_history.copy()
+
+# Acessa cada linha e processa os dados para a geração do gráfico
+for data in df_graf2["Close"]:
+    df_graf2["Close", data] /= first_non_nan(df_graf2["Close", data]) # Normaliza os valores conforme o primeiro valor não nulo
+    df_graf2["Close", data] *= 100 # Transforma a variação em dados percentuais
+    df_graf2["Close", data] -= 100 # Altera o offset, tornando o primeiro valor não nulo como 0, ou seja, como referência de comparação
+
+# Configura e realiza o plot do gráfico
+fig = df_graf2["Close"].plot(zorder=2) # Define os dados a serem plotados
+plt.title("Variação relativa dos ativos no último ano", fontdict={'fontsize': 20}, pad=20) # Define o título
+plt.ylabel("Variação Percentual", fontdict={'fontsize': 14}, labelpad=10) # Define o rótulo do eixo y
+plt.xlabel("Variação do tempo", fontdict={'fontsize': 14}, labelpad=10) # Define o rótulo do eixo x
+plt.grid(color="#DCDCDC", zorder=0) # Configura a exibição de linhas de grade
+fig.axhline(y=0, linestyle="dashed", color="#000000", zorder=0) # Adiciona uma linha de referência em x=0
+plt.legend(loc=(1.02, 0)) # Move a legenda do gráfico para o exterior da área de exibição
+plt.tick_params(which="both", direction="inout") # Edita os marcadores de cada eixo
+
+plt.savefig("grafico_2.png", dpi=75, bbox_inches='tight') # Salva o gráfico gerado em "grafico_1.png"
+
+# Retira a exibição das linhas de grade da planilha
+ws.sheet_view.showGridLines = False
+
+# Abre a imagem gerada que contém o QR code
+img = Image("grafico_2.png")
+
+# Adiciona a imagem à tabela, ancorada sobre a célula de coordenadas (2, 2) -> B2
+ws.add_image(img, ws.cell(2, 2).coordinate)
+
+# Worksheet "Gráfico 3"
+###############################################################################
+
+# Cria uma nova Worksheet chamada "Gráfico 3"
+ws = wb.create_sheet(title="Gráfico 3")
+
+# Cria uma "deep copy" do dataframe que contém o histórico dos ativos
+df_graf3 = data_history.copy()
+
+# Declara listas que serão preenchidas a partir da leitura do historico de dados
+valores_total_carteira = [] # Armazena o valor total da carteira para cada timestamp
+ativos_carteira = list(carteira.values()) # Armazena a lista dos tickers dos ativos
+
+# Itera sorbe cada linha do DataFrame
+for row_index in range(0, df_graf3.shape[0]):
+    # Define uma variável acumuladora como 0
+    valor_carteira_parcial = 0
+    
+    # Itera sobre o valor de cada ação, e acumula o total investido em cada ação na variável "valor_carteira_parcial"
+    for ativo in ativos_carteira: 
+        valor_carteira_parcial += ativo.get("quantidade") * df_graf3["Close", ativo.get("ticker")][row_index]
+        
+    # Após percorrer o valor de cada ativo, adiciona o valor acumulado à lista
+    valores_total_carteira.append(valor_carteira_parcial)
+
+# Cria um novo DataFrame a partir dos timestamps e dos valores totais da carteira para cada timestamp
+df_graf3 = pd.DataFrame(index=df_graf3.index, data=valores_total_carteira)
+
+# Configura e realiza o plot do gráfico
+fig = df_graf3.plot(zorder=2)
+plt.title("Variação do valor total da carteira ao longo do último ano", fontdict={'fontsize': 20}, pad=20) # Define o título
+plt.ylabel("Valor em reais", fontdict={'fontsize': 14}, labelpad=10) # Define o rótulo do eixo y
+plt.xlabel("Variação do tempo", fontdict={'fontsize': 14}, labelpad=10) # Define o rótulo do eixo x
+plt.grid(color="#DCDCDC", zorder=0) # Configura a exibição de linhas de grade
+plt.tick_params(which="both", direction="inout") # Define os parâmetros dos ticks
+fig.get_legend().remove() # Remove as legendas
+plt.savefig("grafico_3.png", dpi=75, bbox_inches='tight') # Salva o gráfico gerado em "grafico_3.png"
+
+# Retira a exibição das linhas de grade da planilha
+ws.sheet_view.showGridLines = False
+
+# Abre a imagem gerada que contém o QR code
+img = Image("grafico_3.png")
+
+# Adiciona a imagem à tabela, ancorada sobre a célula de coordenadas (2, 2) -> B2
+ws.add_image(img, ws.cell(2, 2).coordinate)
+   
 # Worksheet "QR Code"
 ###############################################################################
+
 # Gera um QR Code a partir do "valor_total_carteira" e retorna o caminho até o arquivo gerado
 qrcode_path = qrcodeGenerator.gerar_qrcode(valor_total_carteira)
 
@@ -124,10 +279,16 @@ img = Image(qrcode_path)
 # Adiciona a imagem à tabela, ancorada sobre a célula de coordenadas (4, 3) -> C4
 ws.add_image(img, ws.cell(4, 3).coordinate)
 
-filename = "./rascunhos/Teste-" + str(random.randint(1, 10000)) + ".xlsx"
-# Salva o arquivo
+# Checa se a pasta "Resultados" já existe no diretório
+if not os.path.exists("Resultados"):
+    # Se ainda não existia, ela é criada
+    os.mkdir("Resultados")
+
+# Concatena strings para formar o nome do arquivo, o qual registra, em seu título, o momento em que os seus dados foram processados
+filename = "./Resultados/Resultado - " + str(datetime.now().strftime("%d-%m-%Y %H-%M-%S")) + ".xlsx"
+
+# Salva o WookBook
 wb.save(filename)
 
-
-# DECIDIR ENTRE OS DOIS MODELOS DE DICT E, CONSEQUENTEMENTE, ENTRE UM OUTPUT COM 1 OU 2 TABELAS
 # PROTEGER A TABELA?
+# RECEBER DO ABNER APENAS A COLUNA CLOSE
