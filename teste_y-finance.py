39892cc2640e32cb4c1bf7491648dd668a5fa42e
AbPCV <abnerlucas.pcv.12@gmail.com>
2022-05-28 16:55:39 -0300

    Desenvolva o código atual na forma de funções que retornem os dados necessários e o histórico

--- teste_y-finance.py
+++ teste_y-finance.py
@@ -57,59 +57,54 @@ carteira_modelo = {
         "quantidade": 20,
         "tipo": "Ação",
     },
-    "AMZN":{
+    "STNE":{
         "ticker": "AMZN",
         "quantidade": 10,
         "tipo": "Ação",
     },
-    " ":{
+    "AMZN":{
         "ticker": "STNE",
         "quantidade": 20,
         "tipo": "Ação",
     },
 }
 
-#lista das chaves do dicionário modelo
-lista_nomes = list(carteira_modelo.keys())
 
-#transformando a lista em string
-string_nomes = ' '.join(lista_nomes)
+def nova_carteira(carteira_modelo):
+    #transformando a lista de chaves em string
+    string_nomes = ' '.join(list(carteira_modelo.keys()))
 
-#utilizando o nome das chaves como Ticker de cada ativo
-info_tickers = Ticker(string_nomes)
+    #utilizando o nome das chaves como Ticker de cada ativo
+    info_tickers = Ticker(string_nomes)
 
-#utilizando os nomes dos ativos como Tickers para acessar as informações
-dict_tickers_dados = info_tickers.price
+    #utilizando os nomes dos ativos como Tickers para acessar as informações
+    dict_tickers_dados = info_tickers.price
 
-#looping para acessar os dados necessários através dos dicionário e atualizando o modelo para ser utilizado no próximo passo do projeto
-for ticker in lista_nomes:
-    try:
-        nome_ticker = dict_tickers_dados[ticker]['shortName'] #KeyError nessa linha quando a chave estiver vazia! #TypeError para nome inválido!
-    except KeyError:
-        print("Chave vazia!")
-        del carteira_modelo[ticker]
-        #destruir parte do dicionário afetada
-        Continue
-    except TypeError:
-        print("Nome inválido!")
-        del carteira_modelo[ticker]
-        #destruir parte do dicionário afetada
-        Continue
+    #looping para acessar os dados necessários através dos dicionário e atualizando o modelo para ser utilizado no próximo passo do projeto
+    for ticker in list(carteira_modelo.keys()):
+        try:
+            nome_ticker = dict_tickers_dados[ticker]['shortName'] #KeyError nessa linha quando a chave estiver vazia! #TypeError para nome inválido!
+        except KeyError:
+            del carteira_modelo[ticker] #destruir parte do dicionário afetada
+            Continue
+        except TypeError:
+            del carteira_modelo[ticker] #destruir parte do dicionário afetada
+            Continue
+        else:
+            #caso não ocorra erros será executado normalmente
+            nome_ticker = dict_tickers_dados[ticker]['shortName']
+            valor_atualizado = dict_tickers_dados[ticker]['regularMarketPrice']
+            valor_total = valor_atualizado * carteira_modelo[ticker]["quantidade"]
+            dict_novo_dados = {"nome": nome_ticker, "valor_unitario": valor_atualizado, "valor_total": valor_total}
+            carteira_modelo[ticker].update(dict_novo_dados)
+    #verificação da carteira caso ela seja vazia
+    if carteira_modelo == {}:
+        return("Alerta de dicionario vazio!")
     else:
-        nome_ticker = dict_tickers_dados[ticker]['shortName']
-        valor_atualizado = dict_tickers_dados[ticker]['regularMarketPrice']
-        valor_total = valor_atualizado * carteira_modelo[ticker]["quantidade"]
-        dict_novo_dados = {"nome": nome_ticker, "valor_unitario": valor_atualizado, "valor_total": valor_total}
-        carteira_modelo[ticker].update(dict_novo_dados)
-        print("ok")
+        #resgatando os dados na forma de um dataframe de cada ticker
+        dados_historico = yf.download(list(carteira_modelo.keys()), period = "1y", interval="1d")
+        return carteira_modelo, dados_historico
 
-print(carteira_modelo)
+print(nova_carteira(carteira_modelo))
 
-#resgatando os dados na forma de um dataframe de cada ticker
 
-if carteira_modelo == {}:
-    print("dicionario vazio")
-else:
-    dados_historico = yf.download(list(carteira_modelo.keys()), period = "1y", interval="1d")
-    print(dados_historico.columns)
-    print(dados_historico)
