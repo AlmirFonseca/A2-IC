03b14a054c54515aef08730bcd8f55dfff1d9056
AbPCV <abnerlucas.pcv.12@gmail.com>
2022-05-28 00:42:02 -0300

    Atualize versão do dicionário utilizado e retorne os itens necessários na forma de um dicionário atualizado por meio do yfinance

--- teste_y-finance.py
+++ teste_y-finance.py
@@ -1,27 +1,28 @@
 from tkinter import N
 import yfinance as yf
 import pandas as pd
+from yahooquery import Ticker
 
-#Pequena simulação de preenchimento de dicionário partir de quantidade de tipo de moedas e
-#seus nomes.
+#Pequena simulação de preenchimento de dicionário partir dos dados recebidos do scrapping.
 
 #Pequena simulação de consulta de valores através de dict
 
 #necessário tratamento de exceções e falhas
 
-#não consegui lidar com as moedas ainda
+#função info sem yahooquery é inviável!
 
-#ticker de moeda e ticker de ação
+#ticker é importantíssimo!
 
 #função para encontrar o current price da ação <regularMarketPrice>
 
+#carteira_modelo final
 '''
 carteira_modelo = {
     AMZN:{
         "ticker": "AMZN",
         "quantidade": 10,
         "tipo": "Ação",
-        "nome": "Amazon.com, Inc.",
+        "nome": "Amazon.com, Inc.", ---> desistir por agora
         "valor_unitário": 1254,
         "valor_total": 12540
     }, 
@@ -29,48 +30,46 @@ carteira_modelo = {
         "ticker": "STNE",
         "quantidade": 20,
         "tipo": "Ação",
-        "nome": "StoneCo Ltd.",
+        "nome": "StoneCo Ltd.", ---> desistir por agora
         "valor_unitário": 1254,
         "valor_total": 25080,
     },      
 }
 '''
+#carteira_modelo inicial
+carteira_modelo = {
+    "AMZN":{
+        "ticker": "AMZN",
+        "quantidade": 10,
+        "tipo": "Ação",
+    }, 
+    "STNE":{
+        "ticker": "STNE",
+        "quantidade": 20,
+        "tipo": "Ação",
+    },      
+}
+
+#lista das chaves do dicionário modelo
+lista_nomes = list(carteira_modelo.keys())
+
+#transformando a lista em string
+string_nomes = ' '.join(lista_nomes)
+
+#utilizando o nome das chaves como Ticker de cada ativo
+info_tickers = Ticker(string_nomes)
+
+#utilizando os nomes dos ativos como Tickers para acessar as informações
+dict_tickers_dados = info_tickers.price
+
+#looping para acessar os dados necessários através dos dicionário e atualizando o modelo para ser utilizado no próximo passo do projeto
+for ticker in lista_nomes:
+    nome_ticker = dict_tickers_dados[ticker]['longName']
+    valor_atualizado = dict_tickers_dados[ticker]['regularMarketPrice']
+    valor_total = valor_atualizado * carteira_modelo[ticker]["quantidade"]
+    dict_novo_dados = {"nome": nome_ticker, "valor_unitario": valor_atualizado, "valor_total": valor_total}
+    carteira_modelo[ticker].update(dict_novo_dados)
+
+print(carteira_modelo)
 
 
-
-
-'''
-while contador < quantidade_moeda:
-    tipo_moeda = input("Insira o ticker das moedas: ")
-    dict_dado['moeda'].append(tipo_moeda)
-    contador+=1
-
-contador = 0
-
-quantidade_ticker = int(input("Quantidade de tipos de ação: "))
-while contador < quantidade_ticker:
-    tipo_ticker = input("Insira o ticker das ações: ")
-    dict_dado['ticker'].append(tipo_ticker)
-    contador+=1
-
-contador = 0
-
-print(dict_dado)
-
-while contador < quantidade_ticker:
-    ticker = yf.Ticker(dict_dado['ticker'][contador])
-    ticker_historical = ticker.history(start="2022-05-05", end="2022-06-05", interval="1d")
-    print(ticker_historical)
-    contador+=1
-
-contador = 0
-
-#possível código para moedas
-
-while contador < quantidade_moeda:
-    ticker = yf.Ticker(dict_dado['moeda'][contador])
-    ticker_historical = ticker.history(start="2022-05-05", end="2022-06-05", interval="1d")
-    print(ticker_historical)
-    contador+=1
-
-'''
