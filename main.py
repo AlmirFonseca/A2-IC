0139c8591076e4bffd22991633ef5d1709ca0444
MarianaLima2000 <marianalimab.2000@gmail.com>
2022-05-30 10:07:00 -0300

    faz ajustes de finalização no arquivo main

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from feat_yfinance import analisar_carteira, obter_historico_ativos
 from openpyxl_generator import gerar_xlsx
 
 # Função para construir a carteira e retorna dicioário com papéis e quantidades a partir de link do site.
-carteira = constroi_objeto_carteira("https://marianalima2000.github.io/A2-IC/carteira.html")
+url = str(input('Digite o url da carteira desejada \n'))
+print(url)
+carteira = constroi_objeto_carteira(url)
 
 # Funções que tratam os valores e constroem o histórico dos ativos a partir da lib yfinance
 analisar_carteira(carteira)
-historico_carteira = obter_historico_ativos(carteira)
+if len(carteira) == 0:
+    print("Não há ativos válidos na carteira, sem retorno nos Resultados.")
+else:
+    historico_carteira = obter_historico_ativos(carteira)
 
-#Função que gera os resultados a partir da carteira atual e do histórico gerado anteriormente.
-gerar_xlsx(carteira, historico_carteira)
+    #Função que gera os resultados a partir da carteira atual e do histórico gerado anteriormente.
+    gerar_xlsx(carteira, historico_carteira)
