07e0a606af62e6e0474271102b8341621c5935c9
AlmirFonseca <almirfonseca60@gmail.com>
2022-06-08 20:51:41 -0300

    Adicione validação da URL inserida

---
+++ interface.py
@@ -0,0 +1,28 @@
+import validators
+
+# Define a string a ser impressa como cabeçalho no terminal
+cabecalho = "\n" + "*"*20 + " Analisador de investimentos " + "*"*20 + "\n"
+
+# Função que recebe entrada de url e checa a sua validez
+def entrada_url():
+    # Inicializa uma variável sentinela, que irá controlar a repetição abaixo
+    url_valida = False
+    # Enquanto não houver uma entrada de URL válida, o bloco abaixo se repete
+    while url_valida != True:
+        # Aguarda a inserção de uma URL com a carteira de ativos a ser analisada
+        url_entrada = str(input('Insira o URL da carteira de investimentos a ser analisada: \n'))
+        
+        # Caso nenhum valor seja inserido
+        if url_entrada == "":
+            print("\nNenhuma URL foi inserida. Por favor, tente novamente")
+            continue
+        
+        # Checa se a URL é válida
+        url_valida = validators.url(url_entrada)
+        
+        # Se a URL for válida, a função a retorna
+        if url_valida == True:
+            print("\nAguarde. Estamos analisando sua carteira...")
+            return url_entrada
+        else:
+            print("\nURL inválida! Por favor, tente novamente")
--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import os
 from scrapping import constroi_objeto_carteira
 from feat_yfinance import analisar_carteira, obter_historico_ativos, converter_valores_BRL
 from openpyxl_generator import gerar_xlsx
+from interface import cabecalho, entrada_url
 
-# Aguarda a inserção de uma url com a carteira de ativos a ser analisada
-url = str(input('Digite o url da carteira desejada: \n'))
+# Imprime um cabeçalho no terminal
+print(cabecalho)
+
+# Função que recebe a entrada de uma url, e checa se a url é válida. Se sim, retorna-a
+url = entrada_url()
 
 # Função para construir a carteira e retorna dicionário com papéis e quantidades a partir de link do site.
 carteira = constroi_objeto_carteira(url)
@@ -14,7 +18,7 @@ carteira, tipos_moedas_estrangeiras = analisar_carteira(carteira)
 
 # Caso, após a análise, não restem ativos válidos na carteira
 if len(carteira) == 0:
-    print("Não há ativos válidos na carteira, sem retorno nos Resultados.")
+    print("Não há ativos válidos na carteira. Nenhum resultado foi produzido.")
 else:
     # Funções que constroe o histórico de valores dos ativos a partir da lib yfinance
     historico_carteira = obter_historico_ativos(carteira)
@@ -25,6 +29,9 @@ else:
 
     #Função que gera os resultados a partir da carteira atual e do histórico gerado anteriormente.
     caminho_arquivo_resultados = gerar_xlsx(carteira, historico_carteira)
+    
+    # Imprime o local onde está salvo o arquivo gerado a partir da análise da carteira
+    print("Os resultados da análise da sua carteira foram gerados e se encontram em: \n", caminho_arquivo_resultados)
     
     # Abre o arquivo de resultados gerado pela função anterior
     #os.startfile(caminho_arquivo_resultados)
